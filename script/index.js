1f0fcc1a976dd3aac97c5a8aff7ee30600572197
Karina Nichiporuk <nichiporuk.karina@yandex.ru>
2023-02-11 13:11:58 +0300

    ревью

--- script/index.js
+++ script/index.js
@@ -1,41 +1,40 @@
-
 const popup = document.querySelector('.popup');
 const form = document.querySelector('.popup__form');
 const username = document.querySelector('.profile__title');
 const aboutUser = document.querySelector('.profile__text');
 const editButton = document.querySelector('.profile__button-edit');
 const closeButton = document.querySelector('.popup__close');
+const inputName = form.querySelector('#input-name');
+const inputAbout = form.querySelector('#input-about');
 
 const openPopup = () => {
   popup.classList.add('popup_opened');
 
-  form[0].value = username.textContent;
-  form[1].value = aboutUser.textContent;
+  inputName.value = username.textContent;
+  inputAbout.value = aboutUser.textContent;
 };
-
 
 const closePopup = () => {
   popup.classList.remove('popup_opened');
 };
 
-const overlayClickHandler = (event) => {
+/* const overlayClickHandler = (event) => {
   if (event.target === event.currentTarget) {
     closePopup();
   }
-};
-
+}; */
 
 const updateData = (event) => {
   event.preventDefault();
 
-  username.textContent = form[0].value; // input username
-  aboutUser.textContent = form[1].value; // input job
+  username.textContent = inputName.value;
+  aboutUser.textContent = inputAbout.value;
 
   closePopup();
 };
 
 editButton.addEventListener('click', openPopup);
-closeButton.addEventListener("click", closePopup);
+closeButton.addEventListener('click', closePopup);
 
 /* popup.addEventListener('click', overlayClickHandler); */
 
