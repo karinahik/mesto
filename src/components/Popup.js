fbff92ba93a5b6c8f9bc5ca01a71b5453e12830d
Karina Nichiporuk <nichiporuk.karina@yandex.ru>
2023-04-12 16:36:11 +0700

    fix: images

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,21 +1,21 @@
 export default class Popup {
     constructor(popupSelector) {
         this._popup = document.querySelector(popupSelector)
-        this._handlerEscClose = this._handleEscClose.bind(this)
+        this._handleEscClose = this._handleEscClose.bind(this)
     }
 
     open() {
         this._popup.classList.add('popup_opened')
-        document.addEventListener('keydown', (evt) => {
-            this._handleEscClose(evt)
-        })
+        document.addEventListener('keydown',
+            this._handleEscClose
+        )
     }
 
     close() {
         this._popup.classList.remove('popup_opened')
-        document.removeEventListener('keydown', (evt) => {
-            this._handleEscClose(evt)
-        })
+        document.removeEventListener('keydown',
+            this._handleEscClose
+        )
     }
 
     _handleEscClose(evt) {
